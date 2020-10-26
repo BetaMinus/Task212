dfcea9799d2cba718713fa043508e1cbdf038cd1
Leonid Simukov <pcjmeil@gmail.com>
2020-10-26 21:57:51 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,9 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+//@Scope("prototype")
+public class Cat {
+    public Cat() {
+    }
+}
